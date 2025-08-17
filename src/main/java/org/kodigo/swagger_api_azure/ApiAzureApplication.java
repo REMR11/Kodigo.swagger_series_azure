efdcfd0d115a9848934026adf2d351a68c53bc92
REMR11 <ronaldmejia0211@gmail.com>
2025-08-17 15:03:24 -0600

    Feat:Proyecto base

---
+++ src/main/java/org/kodigo/swagger_api_azure/ApiAzureApplication.java
@@ -0,0 +1,13 @@
+package org.kodigo.swagger_api_azure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAzureApplication.class, args);
+	}
+
+}
